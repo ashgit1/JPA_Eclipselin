b1b5c49eb3a89aa88e17377a372c35f55cb0cb32
Ashishkumar Gupta <akumar9@walmartlabs.com>
2020-06-19 22:22:29 +0530

    adding staff, teaching and non-teaching staff classed. Single Table strategy

---
+++ src/com/ashish/eclipselink/entity/NonTeachingStaff.java
@@ -0,0 +1,31 @@
+package com.ashish.eclipselink.entity;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "NS")
+public class NonTeachingStaff extends Staff {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String areaexpertise;
+
+	public NonTeachingStaff(int sid, String sname, String areaexpertise) {
+		super(sid, sname);
+		this.areaexpertise = areaexpertise;
+	}
+
+	public NonTeachingStaff() {
+		super();
+	}
+
+	public String getAreaexpertise() {
+		return areaexpertise;
+	}
+
+	public void setAreaexpertise(String areaexpertise) {
+		this.areaexpertise = areaexpertise;
+	}
+
+}
